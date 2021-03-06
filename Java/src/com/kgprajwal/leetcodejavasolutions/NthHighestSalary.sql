7baf76e3d8497a9db73c8d2dd4c9a6c3a3e51353
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-03-06 20:29:14 +0530

    Problem 176 & 177

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/NthHighestSalary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N=N-1;
+  RETURN (
+      SELECT (
+        SELECT DISTINCT Salary
+          FROM Employee
+          GROUP BY Salary
+          ORDER BY Salary DESC
+          LIMIT 1 OFFSET N
+      )
+  );
+END
