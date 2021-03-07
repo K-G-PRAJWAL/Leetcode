8db034df115fe464868908df1d29c003b8239bfc
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-03-07 21:58:07 +0530

    Problem 178 & 179

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/LargestNumber.java
@@ -0,0 +1,32 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumber {
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        }
+    }
+
+    public String largestNumber(int[] nums) {
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(asStrs, new LargerNumberComparator());
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+        return largestNumberStr;
+    }
+}
+}
