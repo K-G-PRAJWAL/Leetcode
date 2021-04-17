a8f5c90e166de3c6e133280bc292ae7767dca4fb
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-04-17 21:51:45 +0530

    Problem 279 & 283

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/MoveZeroes.java
@@ -0,0 +1,14 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) nums[index++] = num;
+        }
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+}
