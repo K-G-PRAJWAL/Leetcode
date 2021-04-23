1f8330f1cba06556a8a63a36408ee3e7e5834ed8
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-04-23 20:53:37 +0530

    Problem 338 & 344

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/CountingBits.java
@@ -0,0 +1,15 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class CountingBits {
+    public int[] countBits(int num) {
+        if (num == 0) return new int[1];
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < num + 1; i++) {
+            if (i % 2 == 0) dp[i] = dp[i / 2];
+            else dp[i] = dp[i / 2] + 1;
+        }
+        return dp;
+    }
+}
---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/ReverseString.java
@@ -0,0 +1,12 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l++] = s[r];
+            s[r--] = tmp;
+        }
+    }
+}
