e8dc497674411e6fe1b411797f3ffc9d49673587
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-04-29 20:54:09 +0530

    Problem 303 & 304

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/RangeSumQueryImmutable.java
@@ -0,0 +1,16 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class RangeSumQueryImmutable {
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
