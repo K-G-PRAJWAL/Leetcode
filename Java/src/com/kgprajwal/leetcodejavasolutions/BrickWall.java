1094824f6b6c3f6c4f4a2ac110a5e67f6d68b08e
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2021-06-10 22:10:54 +0530

    Problem 553 & 554

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/BrickWall.java
@@ -0,0 +1,21 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BrickWall {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap();
+        int count = 0;
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                count = Math.max(count, map.get(sum));
+            }
+        }
+        return wall.size() - count;
+    }
+}
