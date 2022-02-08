0ca8e3ff862a7d90b38faec12eb59e1161bd7fe6
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2022-02-08 21:11:33 +0530

    Problem 1823

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,11 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class FindTheWinnerOfTheCircularGame {
+    public int findTheWinner(int n, int k) {
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = (ans + k) % i;
+        }
+        return ans + 1;
+    }
+}
