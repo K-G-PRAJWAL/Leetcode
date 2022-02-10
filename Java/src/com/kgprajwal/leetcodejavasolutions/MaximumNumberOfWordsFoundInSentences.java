3386d6fa1340fd2dd5a4e27ced04a3cdf12e7de6
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2022-02-10 20:41:07 +0530

    Problem 2114

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,11 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+public class MaximumNumberOfWordsFoundInSentences {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+        for (int i = 0; i < sentences.length; i++) {
+            max = Math.max(max, sentences[i].split(" ").length);
+        }
+        return max;
+    }
+}
