765f34be8d5fd959bab75c696a57368537677258
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2022-03-11 19:59:28 +0530

    Problem 872

---
+++ Java/src/com/kgprajwal/leetcodejavasolutions/LeaSimilarTrees.java
@@ -0,0 +1,21 @@
+package com.kgprajwal.leetcodejavasolutions;
+
+import java.util.ArrayList;
+
+public class LeaSimilarTrees {
+    private void dfs(TreeNode root, ArrayList<Integer> arr) {
+        if (root != null) {
+            if (root.left == null && root.right == null) arr.add(root.val);
+            dfs(root.left, arr);
+            dfs(root.right, arr);
+        }
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        dfs(root1, arr1);
+        dfs(root2, arr2);
+        return arr1.equals(arr2);
+    }
+}
