dcec8f084249707210994388945a6b207900dd7a
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2023-02-21 20:20:21 +0530

    540-Single-Element-in-a-Sorted-Array.py - Another Approach

--- Python/540-Single-Element-in-a-Sorted-Array.py
+++ Python/540-Single-Element-in-a-Sorted-Array.py
@@ -15,3 +15,15 @@ class Solution:
                 r -= 2
             else:
                 return nums[r]
+
+        # Another approach
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if m % 2 == 1:
+                m -= 1
+            if nums[m] != nums[m + 1]:
+                r = m
+            else:
+                l = m + 2
+        return nums[l]
